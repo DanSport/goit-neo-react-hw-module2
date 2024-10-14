c2a54471b458c32cb4a309c548b8d81e4e6a2043
DanSport <bogdan.onoyko@gmail.com>
2024-10-15 00:16:27 +0200

    Initial

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+import Description from "./components/Description/Description";
+import Options from "./components/options/Options";
+import Feedback from "./components/feedback/Feedback";
+import Notification from "./components/notification/Notification";
+
+const App = () => {
+  const getInitialFeedback = () => {
+    const savedFeedback = localStorage.getItem("feedback");
+    return savedFeedback
+      ? JSON.parse(savedFeedback)
+      : { good: 0, neutral: 0, bad: 0 };
+  };
+
+  const [feedback, setFeedback] = useState(getInitialFeedback);
+
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(feedback));
+  }, [feedback]);
+
+  const updateFeedback = (feedbackType) => {
+    setFeedback((prevFeedback) => ({
+      ...prevFeedback,
+      [feedbackType]: prevFeedback[feedbackType] + 1,
+    }));
+  };
+
+  const resetFeedback = () => {
+    setFeedback({ good: 0, neutral: 0, bad: 0 });
+  };
+
+  const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+  const positiveFeedback =
+    totalFeedback > 0 ? Math.round((feedback.good / totalFeedback) * 100) : 0;
+
+  return (
+    <div>
+      <Description />
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        resetFeedback={resetFeedback}
+      />
+      <div className="feedback-container visible">
+        {totalFeedback > 0 ? (
+          <Feedback
+            feedback={feedback}
+            totalFeedback={totalFeedback}
+            positiveFeedback={positiveFeedback}
+          />
+        ) : (
+          <Notification message="No feedback given yet." />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/description/Description.jsx
@@ -0,0 +1,13 @@
+import styles from "./Description.module.css";
+
+const Description = () => {
+  return (
+    <p className={styles.description}>
+      <h1>Sip Happens Café</h1>
+      Please leave your feedback about our service by selecting one of the
+      options below.
+    </p>
+  );
+};
+
+export default Description;
